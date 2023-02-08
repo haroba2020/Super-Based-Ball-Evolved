b7175658477cdce3383931a0886bcf3926ce693f
Fredrik <fredriktheyerdahl@gmail.com>
2023-02-08 21:41:56 +0100

    adding react image compatability and imported non functinal old code

--- src/Play.js
+++ src/Play.js
@@ -1,31 +1,72 @@
 import { Link } from 'react-router-dom'
+import {playerA, playerB} from './playerData'
+import {player1, player1} from './game'
+console.log(playerA)
+export const GIF_DATA = (function () { 
+    let arr = []
+    for(let i = 1 ; i <= 5 ; i++ ){
+        let tmpImg = require( `./assets/${i}.gif`)
+        arr.push(tmpImg)
+    }
+    return arr
+})()
+console.log(playerA)
+
+addEventListener("keydown", function (e) {
+    if (e.key == "d" && playerA.cooldown == false) {
+        player1.setAttribute('src', "../img/player red shoot.gif");
+        playerA.switchCooldown();
+        setTimeout(playerA.returnPlayer, 1000);
+        setTimeout(() => {
+            playerA.switchCooldown()
+        }, 1000)
+        setTimeout(() => {
+            playerA.aHitStart()
+        }, 300)
+    } else if (e.key == "k" && playerB.cooldown == false) {
+        player2.setAttribute('src', "../img/player blue shootgif.gif")
+        playerB.switchCooldown();
+        setTimeout(playerB.returnPlayer, 1000);
+        setTimeout(() => {
+            playerB.switchCooldown()
+        }, 1000)
+        setTimeout(() => {
+            playerB.bhitStart()
+        }, 300)
+    } else if (e.key == " ") {
+        console.log(e.key)
+        game.restartGame()
+    }
+})
+
+console.log(GIF_DATA)
 
 const Play = () => {
     return ( 
         <div className="play">
-            <div class="flex-parent-player">
-                <div class="flex-player gameFinal">
-                    <h1 class="gameFinal play1Stat d-none">WIN</h1>
+            <div className="flex-parent-player">
+                <div className="flex-player gameFinal">
+                    <h1 className="gameFinal play1Stat d-none">WIN</h1>
                 </div>
-                <div class="text-center white-space">
+                <div className="text-center white-space">
                     <Link to="/home" className='title'>Exit</Link>
                 </div>
-                <div class="flex-player">
-                    <h1 class="gameFinal play2Stat d-none">LOSE</h1>
+                <div className="flex-player">
+                    <h1 className="gameFinal play2Stat d-none">LOSE</h1>
                 </div>
             </div>
-
-            <div class="flex-parent-player pt-5">
-                <div class="flex-player">
-
+            
+            <div className="flex-parent-player pt-5">
+                <div className="flex-player">
+                    <img src={GIF_DATA[0]} className='flip player' alt="player1" />
                 </div>
-                <div class="white-space"></div>
-                <div class="flex-player">
-
+                <div className="white-space"></div>
+                <div className="flex-player">
+                    <img src={GIF_DATA[1]} className=' player' alt="player2" />
                 </div>
             </div>
-            <div class="text-center score">
-                <h1 class="score">0</h1>
+            <div className="text-center score">
+                <h1 className="score">0</h1>
             </div>
         </div>
      );
