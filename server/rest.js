9a017426be791100e77b5a4ecb12d0a0929d36ec
blorptopia <vegardso@protonmail.com>
2023-03-28 10:01:39 +0200

    fix: cors

--- server/rest.js
+++ server/rest.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { REST_PORT } from "./constants.js";
+import cors from "cors";
 
 // Routers
 import accountRouter from "./routes/account.js";
 import matchmakingRouter from "./routes/matchmaking.js";
 
 export const app = express();
+app.use(cors());
 
 // Routes
 app.use(accountRouter);
