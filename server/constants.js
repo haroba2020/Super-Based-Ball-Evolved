f52ca89e544e85e2e8f076d8fcc0fe54881e00fd
Fredrik <fredriktheyerdahl@gmail.com>
2023-04-03 14:55:48 +0200

    fixing .env lmao

--- server/constants.js
+++ server/constants.js
@@ -1,6 +1,6 @@
-export const REST_PORT = process.env.REST_PORT || 3001;
-export const RTC_PORT = process.env.RTC_PORT || 3002;
-export const SIGNING_KEY = process.env.SIGNING_KEY || "secretfordev";
-export const DB_URI =
-  process.env.MONGODB_URI ||
-  "mongodb+srv://CyberBoarBot:mGCjmi3xaGbPVCU4@superbasedballdb.sdfo5br.mongodb.net/SBBE?retryWrites=true&w=majority";
+require('dotenv').config({ path: ".env" });
+
+export const REST_PORT = process.env.REST_PORT
+export const RTC_PORT = process.env.RTC_PORT
+export const SIGNING_KEY = process.env.SIGNING_KEY
+export const DB_URI = process.env.MONGODB_URI
