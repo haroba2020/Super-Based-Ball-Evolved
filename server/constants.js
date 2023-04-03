08a6f44936363d6ea699431213e0457929d4ceb1
Fredrik <fredriktheyerdahl@gmail.com>
2023-04-03 15:05:37 +0200

    fixing .env typo

--- server/constants.js
+++ server/constants.js
@@ -1,6 +1,7 @@
 require('dotenv').config({ path: ".env" });
+dotenv.config();
 
 export const REST_PORT = process.env.REST_PORT
 export const RTC_PORT = process.env.RTC_PORT
 export const SIGNING_KEY = process.env.SIGNING_KEY
-export const DB_URI = process.env.MONGODB_URI
+export const DB_URI = process.env.DB_URI
