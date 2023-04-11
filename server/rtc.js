21ea00b2ebf27b05ed50555272bacd05f25b4339
blorptopia <vegardso@protonmail.com>
2023-04-11 12:25:28 +0200

    perf: delete old ws connections

--- server/rtc.js
+++ server/rtc.js
@@ -3,11 +3,20 @@ import { RTC_PORT } from "./constants.js";
 
 export const wss = new WebSocketServer({port: RTC_PORT});
 
-const channels = {};
+export const channels = {};
 
 wss.on("connection", async function onConnect(ws) {
     let channelId = null;
-    
+    ws.on("close", async function onDisconnect() {
+        console.log(`Disconnect from ${channelId}`);
+        if (channelId !== null) {
+            channels[channelId] = channels[channelId].filter(maybeWS => maybeWS !== ws);
+            if (channels[channelId].length === 0) {
+                channels[channelId] = undefined;
+                console.log(`Deleted ${channelId}`);
+            }
+        }
+    });
     ws.on("message", async function onMessage(data) {
         data = data.toString("utf-8");
         if (channelId == null) {
