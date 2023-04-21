4211e9dd0461ced29cc60a88892880116e6f3c5f
blorptopia <vegardso@protonmail.com>
2023-04-21 19:59:59 +0200

    build(cloudflare): add rtc and api domains

--- infra/dns.tf
+++ infra/dns.tf
@@ -2,10 +2,25 @@ provider "cloudflare" {
   api_token = var.cloudflare_api_key
 }
 
-# Create a record
 resource "cloudflare_record" "thugshaker_servers" {
   zone_id = var.cloudflare_zone_id
   name = "thugshaker.servers"
   value = var.server_thugshaker_ip
   type = "A"
+}
+
+resource "cloudflare_record" "api" {
+  zone_id = var.cloudflare_zone_id
+  name = "api"
+  value = var.server_thugshaker_ip
+  type = "A"
+  proxied = true
+}
+
+resource "cloudflare_record" "rtc" {
+  zone_id = var.cloudflare_zone_id
+  name = "rtc"
+  value = var.server_thugshaker_ip
+  type = "A"
+  proxied = true
 }
