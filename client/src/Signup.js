4ce5211ff554ee7a712daacf23e7067dde264093
haroba2020 <fredriktheyerdahl@gmail.com>
2023-04-22 15:01:18 +0200

    adding fetch request fixes

--- client/src/Signup.js
+++ client/src/Signup.js
@@ -1,14 +1,48 @@
+import { apiURL } from "./constants";
+
 const Signup = () => {
+    async function fetching(e){
+        e.preventDefault();
+        console.log(e)
+        console.log('function run')
+        const form = document.querySelector('.styled-form')
+      
+        const email = form.email.value
+        const password = form.password.value
+        const playerName = form.playerName.value
+        try {
+          console.log('try trying')
+          //sender data som lager bruker
+          const res = await fetch(`${apiURL}/@me`,{
+            method: 'post',
+            query: JSON.stringify({email,password,playerName}),
+            headers: {'Content-Type': 'application/json'}
+          })
+          //venter på user data
+          const data = await res.json()
+          console.log(data)
+      
+          // Error handler
+          if(data.errors){
+            console.log(data.errors)
+          }
+      
+        } catch (err) {
+          console.log(err)
+        }
+      }
+    
     return ( 
         <div className="signup">
-            <div class="form-container">
-                <form action="" class="styled-form">
+            <div className="form-container">
+                <form action="" className="styled-form">
                     <input type="text" name="email" id="email" />
-                    <label for="email">Email:</label>
+                    <label htmlFor="email">Email:</label>
                     <input type="text" name="playerName" id="playerName" />
-                    <label for="playerName">Name:</label>
+                    <label htmlFor="playerName">Name:</label>
                     <input type="text" name="password" id="password" />
-                    <label for="password">Password:</label>
+                    <label htmlFor="password">Password:</label>
+                    <button onClick={fetching}>Submit</button>
                 </form>
             </div>
         </div>
